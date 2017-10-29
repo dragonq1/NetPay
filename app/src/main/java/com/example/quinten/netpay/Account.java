84a64e578d2b6e4e5adc46fd2391bdd82713f8c9
dragonq1 <31737624+dragonq1@users.noreply.github.com>
2017-10-29 16:20:44 +0100

    -Start gemaakt aan account pagina

--- app/src/main/java/com/example/quinten/netpay/Account.java
+++ app/src/main/java/com/example/quinten/netpay/Account.java
@@ -1,7 +1,9 @@
 package com.example.quinten.netpay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Account extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        //Vars
+        final TextView txtVoornaam = (TextView) findViewById(R.id.txtVoornaam);
+        final TextView txtAchternaam = (TextView) findViewById(R.id.txtAchternaam);
+
+        //Doorgegegeven vars
+        Intent intent = getIntent();
+        final String strVoornaam = intent.getStringExtra("voornaam");
+        final String strAchternaam = intent.getStringExtra("achternaam");
+
+        //nodige Textviews uitschakelen
+        txtAchternaam.setEnabled(false);
+        txtVoornaam.setEnabled(false);
+
+        //Textviews invullen
+        txtAchternaam.setText(strAchternaam);
+        txtVoornaam.setText(strVoornaam);
+
+
     }
 }
--- app/src/main/java/com/example/quinten/netpay/Menu.java
+++ app/src/main/java/com/example/quinten/netpay/Menu.java
@@ -3,6 +3,7 @@ package com.example.quinten.netpay;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,14 +27,35 @@ public class Menu extends Activity {
         final Button btnOntvangen = (Button) findViewById(R.id.btnOntvangen);
         final Button btnAccount = (Button) findViewById(R.id.btnAccount);
 
+        //Doorgegegeven vars
         Intent intent = getIntent();
+        final String strVoornaam = intent.getStringExtra("voornaam");
+        final String strAchternaam = intent.getStringExtra("achternaam");
+        final String strGebruikersnaam = intent.getStringExtra("gebruikersnaam");
+        final String strSaldo = intent.getStringExtra("saldo");
 
         //Welkom bericht personaliseren
-        String strVoornaam = intent.getStringExtra("voornaam");
+
         String strWelkomBericht = strVoornaam.substring(0,1).toUpperCase() + strVoornaam.substring(1) + "!";
         strWelkomBericht = strWelkomBericht.replace(" ", "");
         strWelkomBericht = "Welkom " + strWelkomBericht;
         txtWelkom.setText(strWelkomBericht);
+
+        //Button listeners
+
+        btnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Account.class);
+                intent.putExtra("gebruikersnaam", strGebruikersnaam);
+                intent.putExtra("voornaam", strVoornaam);
+                intent.putExtra("achternaam", strAchternaam);
+                intent.putExtra("saldo", strSaldo);
+                startActivity(intent);
+            }
+        });
+
+
 
 
 
