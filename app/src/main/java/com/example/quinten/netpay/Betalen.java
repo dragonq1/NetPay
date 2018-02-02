125bebabec1abef0f9aaa0fafbc8e2181f1000ab
dragonq1 <31737624+dragonq1@users.noreply.github.com>
2018-02-02 15:22:07 +0100

    -Betaling method toegevoegd aan betalen en verbonden met alert dialog voor qr betalingen -> getest + werkt -Nieuwe betaling request gemaakt speciaal voor qr betalingen ivm nieuw php script voor updaten db -Checks toegevoegd aan qr betalingen: wachtwoord, velden ingevuld en saldo check

--- app/src/main/java/com/example/quinten/netpay/Betalen.java
+++ app/src/main/java/com/example/quinten/netpay/Betalen.java
@@ -3,23 +3,28 @@ package com.example.quinten.netpay;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.example.quinten.requests.BetalingQRRequest;
 import com.example.quinten.requests.QRCODEInfoRequest;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import static com.example.quinten.netpay.MainActivity.USER_INFO;
 
 public class Betalen extends AppCompatActivity {
 
@@ -39,6 +44,7 @@ public class Betalen extends AppCompatActivity {
         final Button btnQRCode = (Button) findViewById(R.id.btnQRCode);
         final Button btnManueel = (Button) findViewById(R.id.btnManueel);
         final Activity activity = this;
+
 
 
         //Manuel betalen
@@ -79,11 +85,16 @@ public class Betalen extends AppCompatActivity {
             if(result.getContents() == null) {
                 //Geen resultaat -> niets
             }else{
-                try {
-                     getQRCodeInfo(result.getContents());
-                }catch(Exception e) {
-                    Toast.makeText(getApplicationContext(), "ERROR 2!" + " " +  e.getMessage(), Toast.LENGTH_LONG).show();
+                if(result.getContents().startsWith("2247")) {
+                    try {
+                        getQRCodeInfo(result.getContents());
+                    }catch(Exception e) {
+                        Toast.makeText(getApplicationContext(), "ERROR 2!" + " " +  e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "QR-code ongeldig!!", Toast.LENGTH_LONG).show();
                 }
+
             }
         }else{
             Toast.makeText(getApplicationContext(), "Geen QR-code gevonden!", Toast.LENGTH_LONG).show();
@@ -103,30 +114,32 @@ public class Betalen extends AppCompatActivity {
                     boolean success = jsonReponse.getBoolean("success");
 
                     if (success) {
-                        Toast.makeText(getApplicationContext(), "Test 1", Toast.LENGTH_LONG).show();
                         //Terugekregen betaling info ophalen
-                        String strBedrag = jsonReponse.getString("Bedrag");
 
-                        String strGebruikersnaam = jsonReponse.getString("Gebruikersnaam");
-                        String strNaam = jsonReponse.getString("Naam");
-                        String stridCode = jsonReponse.getString("idCode");
-
-                        Toast.makeText(getApplicationContext(), "Test 2", Toast.LENGTH_LONG).show();
+                            String strBedrag = jsonReponse.getString("Bedrag");
+                            String strGebruikersnaam = jsonReponse.getString("Gebruikersnaam");
+                            String strNaam = jsonReponse.getString("Naam");
+                            String stridCode = jsonReponse.getString("idCode");
 
 
-                        String[] QRCodeInfo = new String[4];
-                        QRCodeInfo[0] = strBedrag;
-                        QRCodeInfo[1] = strGebruikersnaam;
-                        QRCodeInfo[2] = strNaam;
-                        QRCodeInfo[3] = stridCode;
+                            String[] QRCodeInfo = new String[4];
+                            QRCodeInfo[0] = strBedrag;
+                            QRCodeInfo[1] = strGebruikersnaam;
+                            QRCodeInfo[2] = strNaam;
+                            QRCodeInfo[3] = stridCode;
 
-                        AlertDialog(QRCodeInfo);
+                            AlertDialog(QRCodeInfo);
 
-                    } else {
-                        if (jsonReponse.getString("success").isEmpty()) {
-                            Toast.makeText(getApplicationContext(), "Er ging iets fout!", Toast.LENGTH_LONG).show();
+
+                    }else if(jsonReponse.getString("error").equals("empty")) {
+                        Toast.makeText(getApplicationContext(), "Geen betaling gevonden!", Toast.LENGTH_LONG).show();
+                    }
+
+                    else {
+                        if (jsonReponse.getString("error").isEmpty()) {
+                            Toast.makeText(getApplicationContext(), "Er ging iets fout!" + jsonReponse.getString("success"), Toast.LENGTH_LONG).show();
                         }else{
-                            Toast.makeText(getApplicationContext(), "Er ging iets fout!" + " " +jsonReponse.getString("success"), Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Er ging iets fout!" + " 1 " +jsonReponse.getString("success") + " 2 " + jsonReponse.getString("error"), Toast.LENGTH_LONG).show();
                         }
                     }
 
@@ -143,43 +156,130 @@ public class Betalen extends AppCompatActivity {
 
     }
 
+    //Betaling methide
+    public void betaling(String gbnOntvanger, final String bedrag, String qrCode) {
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    String success = jsonResponse.getString("success");
+
+                    switch(success) {
+                        case "success":
+                            Toast.makeText(getApplicationContext(), "Transactie geslaagd!", Toast.LENGTH_LONG).show();
+
+
+                            //Saldo aanpassen
+                            SharedPreferences settings = getSharedPreferences(USER_INFO, 0);
+                            SharedPreferences.Editor editor = settings.edit();
+                            double dblBedrag = Double.parseDouble(bedrag);
+                            double dblSaldo = Double.parseDouble(settings.getString("saldo", ""));
+
+                            String strNieuwSaldo =  String.valueOf((dblSaldo - dblBedrag));
+                            editor.putString("saldo",  strNieuwSaldo);
+                            editor.apply();
+
+
+                            //Terug naar betaling activity gaan
+                            Intent intent = new Intent(getApplicationContext(), Betalen.class);
+                            startActivity(intent);
+                            break;
+                        case "Statement 2":
+                            Toast.makeText(getApplicationContext(), "Statement 2", Toast.LENGTH_LONG).show();
+                            break;
+                        case "Statement 3":
+                            Toast.makeText(getApplicationContext(), "Statement 3", Toast.LENGTH_LONG).show();
+                            break;
+                        case "false":
+                            Toast.makeText(getApplicationContext(), "false", Toast.LENGTH_LONG).show();
+                            break;
+                        default:
+                            Toast.makeText(getApplicationContext(), success, Toast.LENGTH_LONG).show();
+                            break;
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "ERROR 4" + " " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        SharedPreferences settings = getSharedPreferences(USER_INFO, 0);
+        String gebruikersnaamBetaler = settings.getString("gebruikersnaam", "");
+
+        BetalingQRRequest betalingQRRequest = new BetalingQRRequest(gbnOntvanger, gebruikersnaamBetaler, bedrag, qrCode, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(Betalen.this);
+        queue.add(betalingQRRequest);
+    }
+
     //Alert dialog creeëren en tonen
-    public void AlertDialog(String[] QRCodeInfo) {
+    public void AlertDialog(final String[] QRCodeInfo) {
 
         try{
-            String strBedrag = QRCodeInfo[0].replace(".", ",") + " euro";
-            String strGebruikersnaam = QRCodeInfo[1];
+            final String strBedrag = QRCodeInfo[0];
+            final String strBedragTV = strBedrag.replace(".", ",") + " euro";
+            final String strGebruikersnaam = QRCodeInfo[1];
             String strNaam = QRCodeInfo[2].replace("  ", " ");
-            String stridCode = QRCodeInfo[3];
+            final String stridCode = QRCodeInfo[3];
 
-            View v = getLayoutInflater().inflate(R.layout.dialogbox_betaling, null);
-            AlertDialog.Builder builder = new AlertDialog.Builder(Betalen.this);
+            //Saldo en naam checken + vars
+            final SharedPreferences settings = getSharedPreferences(USER_INFO, 0);
+            double dblSaldo = Double.parseDouble(settings.getString("saldo", ""));
+            double dblBedrag = Double.parseDouble(strBedrag);
+            final String strWachtwoord = settings.getString("wachtwoord", "");
+            String strgbnBetaler = settings.getString("gebruikersnaam", "");
 
-            TextView tvBedrag =  v.findViewById(R.id.txtBedragBetalingQR);
-            TextView tvOntvanger =  v.findViewById(R.id.txtBegunstigdeBetalingQR);
+            if(dblSaldo < dblBedrag) {
+                Toast.makeText(getApplicationContext(), "Saldo ontoerijkend!", Toast.LENGTH_LONG).show();
+            }
+            else if(strgbnBetaler.equals(strGebruikersnaam)) {
+                Toast.makeText(getApplicationContext(), "Je kan geen betaling aan jezelf doen!", Toast.LENGTH_LONG).show();
+            }else{
 
-            builder.setTitle("Nieuwe betaling");
-            tvBedrag.setText(strBedrag);
-            tvOntvanger.setText(strNaam);
+                View v = getLayoutInflater().inflate(R.layout.dialogbox_betaling, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(Betalen.this);
 
-            builder.setPositiveButton("Bevestigen", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+                TextView tvBedrag = v.findViewById(R.id.txtBedragBetalingQR);
+                TextView tvOntvanger = v.findViewById(R.id.txtBegunstigdeBetalingQR);
+                final EditText txtWachtwoord = v.findViewById(R.id.txtWachtwoordBetalingQR);
 
-                }
-            }).setNegativeButton("Annuleren", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-
-                }
-            });
-
+                builder.setTitle("Nieuwe betaling");
+                tvBedrag.setText(strBedragTV);
+                tvOntvanger.setText(strNaam);
 
 
-            builder.setView(v);
 
-            AlertDialog dialog = builder.create();
-            dialog.show();
+                builder.setPositiveButton("Bevestigen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Wachtwoord en veld nakijken
+                        final String strWachtwoordBox = txtWachtwoord.getText().toString();
+                        if(txtWachtwoord.getText().toString().equals("")) {
+                            Toast.makeText(getApplicationContext(), "Vul je wachtwoord in!", Toast.LENGTH_LONG).show();
+                            AlertDialog(QRCodeInfo);
+                        }else if(!(strWachtwoordBox.equals(strWachtwoord))) {
+                            Toast.makeText(getApplicationContext(), "Wachtwoord incorrect!", Toast.LENGTH_LONG).show();
+                            AlertDialog(QRCodeInfo);
+                        }else{
+                            betaling(strGebruikersnaam, strBedrag, stridCode);
+                        }
+
+                    }
+                }).setNegativeButton("Annuleren", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Niets
+                    }
+                });
+
+
+                builder.setView(v);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
 
         }catch(Exception e) {
             Toast.makeText(getApplicationContext(), "ERROR 3!" + " " +  e.getMessage(), Toast.LENGTH_LONG).show();
