b8b907d230af013bfc2133fa7290850c4707f682
dragonq1 <31737624+dragonq1@users.noreply.github.com>
2018-04-02 17:26:16 +0200

    -Notificaties toegevoegd

--- app/src/main/java/com/example/quinten/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/quinten/Services/MyFirebaseMessagingService.java
@@ -1,13 +1,16 @@
 package com.example.quinten.Services;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.quinten.netpay.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    public MyFirebaseMessagingService() {
-    }
 
     private static final String TAG = "FCM Service";
     @Override
@@ -18,5 +21,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContentText(remoteMessage.getNotification().getBody());
+        mBuilder.setContentTitle(remoteMessage.getNotification().getTitle());
+        mBuilder.setSmallIcon(R.drawable.ic_not_icon);
+        mBuilder.setColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        mBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
     }
 }
